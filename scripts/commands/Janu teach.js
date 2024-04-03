d82ea17d5eebf43daf9c2ec92bb58ab934f7bed2
Mahabub443 <164204019+Mahabub443@users.noreply.github.com>
2024-04-03 14:49:32 +0600

    Create Janu teach.js

---
+++ scripts/commands/Janu teach.js
@@ -0,0 +1,45 @@
+const axios = require("axios");
+
+module.exports.config = {
+    name: "teach2",
+    version: "1.0.0",
+    permission: 0,
+    credits: "Rahad",
+    description: "Teach to sim",
+    prefix: true, 
+    category: "sim simi fun", 
+    usages: "[ask] => [answer]",
+    cooldowns: 5,
+    dependencies: {}
+};
+
+module.exports.run = async function({ api, event, args }) {
+    const { threadID, messageID } = event;
+    const input = args.join(" ").split("=>");
+
+    if (input.length < 2) {
+        if (args.length === 0) {
+            return api.sendMessage("Follow this steps to add teach on janu.js\n Usage: teach [ask] => [answer]", threadID);
+        } else if (args.join(" ").includes("=>")) {
+            return api.sendMessage("Please provide both a question and an answer.", threadID);
+        } else {
+            return api.sendMessage("Please use '=>' character to separate the question and answer.", threadID);
+        }
+    }
+
+    const teachQuery = input[0].trim();
+    const ansQuery = input[1].trim();
+
+    try {
+        const response = await axios.get(`http://nl2-4.deploy.sbs:2016/sim?teach=${encodeURIComponent(teachQuery)}&ans=${encodeURIComponent(ansQuery)}`);
+
+        if (response.status >= 200 && response.status < 300) {
+            api.sendMessage(`Teaching successful on Janu.js! Question: ${teachQuery}, Answer: ${ansQuery}`, threadID, messageID);
+        } else {
+            api.sendMessage("An error occurred while teaching janu.js! Please try again later.", threadID);
+        }
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("An error occurred while fetching the data.", threadID);
+    }
+};
